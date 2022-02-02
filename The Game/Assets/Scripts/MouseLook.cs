b18383e3aa57120aef8811efa854ce65b8c67f76
domibron2 <98326467+domibron2@users.noreply.github.com>
2022-02-02 14:55:31 +0000

    added gun for testing

--- The Game/Assets/Scripts/MouseLook.cs
+++ The Game/Assets/Scripts/MouseLook.cs
@@ -18,7 +18,7 @@ public class MouseLook : MonoBehaviour
     }
 
     // Update is called once per frame
-    void Update()
+    void LateUpdate()
     {
         float mouseX = Input.GetAxis("Mouse X") *  mouseSensitivity * Time.deltaTime;
         float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
